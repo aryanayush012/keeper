8ba2d2c0f176875a2d9a35d3afd4a3e404a9b543
Ayush Aryan <76223671+aryanayush012@users.noreply.github.com>
2023-05-28 01:26:07 +0530

    keeper version1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from '../src/components/Header';
+import App from './components/App';
+
+ReactDOM.render(
+  <div>
+    <App></App>
+  </div>,
+  document.getElementById("root")
+);
